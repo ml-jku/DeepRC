9898edec361b8c6a4b5ef7f3c0d8ff511ee970d4
Michael Widrich <widrich@ml.jku.at>
2021-01-28 12:40:00 +0100

    better error message if download fails or user aborts download;

--- deeprc/utils.py
+++ deeprc/utils.py
@@ -18,13 +18,19 @@ def user_confirmation(text: str = "Continue?", continue_if: str = 'y', abort_if:
         if user_input == continue_if:
             break
         elif user_input == abort_if:
-            exit()
+            exit("Session terminated by user.")
 
 
 def url_get(url: str, dst: str, verbose: bool = True):
     """Download file from `url` to file `dst`"""
     stream = requests.get(url, stream=True)
-    stream_size = int(stream.headers['Content-Length'])
+    try:
+        stream_size = int(stream.headers['Content-Length'])
+    except KeyError:
+        raise FileNotFoundError(f"Sorry, the URL {url} could not be reached. "
+                                f"Either your connection has a problem or the server is down."
+                                f"Please check your connection, try again later, "
+                                f"or notify me per email if the problem persists.")
     src = stream.raw
     windows = os.name == 'nt'
     copy_bufsize = 1024 * 1024 if windows else 64 * 1024
